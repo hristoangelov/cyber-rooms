ddf10440c05af7173e969789ff4adc3eeff6aa95
Hristo Angelov <hristoanan@gmail.com>
2024-03-02 13:57:47 +0000

    add UI for leaderboard prompt

--- Cyber Rooms/Assets/PhysicsButton.cs
+++ Cyber Rooms/Assets/PhysicsButton.cs
@@ -9,9 +9,11 @@ public class PhysicsButton : MonoBehaviour
     [SerializeField] private TMP_Text validationMessage;
     [SerializeField] private GameObject phishingCorrectAnswers;
     [SerializeField] private GameObject timer;
+    [SerializeField] private GameObject leaderboardPrompt;
     public UnityEvent onRelease;
     private bool isPressed;
     private int timeWeight = 300;
+    private int minimumScore = 20;
     AudioSource sound;
 
     void Start()
@@ -41,6 +43,7 @@ public class PhysicsButton : MonoBehaviour
         validationMessage.SetText("Great job! The attackers just decided that if even possible, attacking your company will be too hard!\nYou are now prepared to defend both your personal and company's data.");
         validationMessage.color = new Color(0, 255, 0, 255);
 
+        leaderboardPrompt.SetActive(true);
         //stop timer and get the final score of the player
         timer.GetComponent<Timer>().StopTimer();
         float finalSCore = CalculateFinalScore();
@@ -52,7 +55,7 @@ public class PhysicsButton : MonoBehaviour
         //get phsihing score and the timer value
         float timeTaken = timer.GetComponent<Timer>().timer;
         int correctAnswers = phishingCorrectAnswers.GetComponent<ScoreTracker>().correct;
-        float finalScore = correctAnswers + (timeWeight / timeTaken);
-        return finalScore*10;
+        float finalScore =  (correctAnswers + (timeWeight / timeTaken))*10;
+        return minimumScore + finalScore;
     }
 }
