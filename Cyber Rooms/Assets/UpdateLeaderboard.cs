cfb14991b37114ae7305f1d942e5ca3d62510876
Hristo Angelov <hristoanan@gmail.com>
2024-03-03 09:58:20 +0000

    add position and score in the leaderboard header

--- Cyber Rooms/Assets/UpdateLeaderboard.cs
+++ Cyber Rooms/Assets/UpdateLeaderboard.cs
@@ -4,8 +4,7 @@ using UnityEngine.Networking;
 using Keyboard;
 using Newtonsoft.Json;
 using TMPro;
-using UnityEngine.SocialPlatforms.Impl;
-using UnityEditor.Rendering;
+using System;
 
 public class UpdateLeaderboard : MonoBehaviour
 {
@@ -13,8 +12,10 @@ public class UpdateLeaderboard : MonoBehaviour
     [SerializeField] private GameObject phishingCorrectAnswers;
     [SerializeField] private GameObject usernamePrompt;
     [SerializeField] private TextMeshProUGUI[] topScores;
+    [SerializeField] private TMP_Text leaderboardHeader;
     private int timeWeight = 300;
     private int minimumScore = 20;
+    public int position;
 
     private float CalculateFinalScore()
     {
@@ -35,6 +36,10 @@ public class UpdateLeaderboard : MonoBehaviour
     {
         yield return SetScore();
         yield return GetScore();
+
+        // update header of leaderboard
+        string score = CalculateFinalScore().ToString();
+        leaderboardHeader.text = $"You scored <i>{score}</i> points and are on <i>position {position}</i>";
     }
 
     public IEnumerator SetScore()
@@ -53,7 +58,7 @@ public class UpdateLeaderboard : MonoBehaviour
     {
         string username = usernamePrompt.GetComponent<KeyboardManager>().outputField.text;
 
-        string url = "http://dreamlo.com/lb/65e0e8f28f40bbbe889ee7be/json/5";
+        string url = "http://dreamlo.com/lb/65e0e8f28f40bbbe889ee7be/json";
 
         using (UnityWebRequest request = UnityWebRequest.Get(url))
         {
@@ -63,7 +68,14 @@ public class UpdateLeaderboard : MonoBehaviour
             DreamloClasses response = JsonConvert.DeserializeObject<DreamloClasses>(responseData);
             UpdateCurrentLeaderboard(response);
 
-            Debug.Log("Get scores");
+            for (int i = 0; i < response.dreamlo.leaderboard.entry.Count; i++)
+            {
+                if (response.dreamlo.leaderboard.entry[i].name == username)
+                {
+                    position = i + 1;
+                    break;
+                }
+            }
         }
     }
 
@@ -74,25 +86,9 @@ public class UpdateLeaderboard : MonoBehaviour
             score.text = "";
         }
 
-        if (response.dreamlo.leaderboard.entry[0].name != null)
+        for (int i = 0; i < Math.Min(response.dreamlo.leaderboard.entry.Count, 5); i++)
         {
-            topScores[0].text = $"1. {response.dreamlo.leaderboard.entry[0].name} - {response.dreamlo.leaderboard.entry[0].score}";
-        }
-        if (response.dreamlo.leaderboard.entry[1].name != null)
-        {
-            topScores[1].text = $"2. {response.dreamlo.leaderboard.entry[1].name} - {response.dreamlo.leaderboard.entry[1].score}";
-        }
-        if (response.dreamlo.leaderboard.entry[2].name != null)
-        {
-            topScores[2].text = $"3. {response.dreamlo.leaderboard.entry[2].name} - {response.dreamlo.leaderboard.entry[2].score}";
-        }
-        if (response.dreamlo.leaderboard.entry[3].name != null)
-        {
-            topScores[3].text = $"4. {response.dreamlo.leaderboard.entry[3].name} - {response.dreamlo.leaderboard.entry[3].score}";
-        }
-        if (response.dreamlo.leaderboard.entry[4].name != null)
-        {
-            topScores[4].text = $"5. {response.dreamlo.leaderboard.entry[4].name} - {response.dreamlo.leaderboard.entry[4].score}";
+            topScores[i].text = $"{i + 1}. {response.dreamlo.leaderboard.entry[i].name} - {response.dreamlo.leaderboard.entry[i].score}";
         }
     }
 
